912f3e84244b62204e00da0ff771824ec113a9da
DanielArape17 <danielarapework@gmail.com>
2025-09-13 13:17:02 -0400

    Update

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ let numeroAleatorio = () => Math.floor(Math.random() * (3));
 
 const jugadorEleccion = document.getElementById("jugadorEleccion");
 const pcEleccion = document.getElementById("pcEleccion");
+const ganados = document.getElementById('ganados');
+const empatados = document.getElementById('empatados');
+const perdidos = document.getElementById('perdidos');
+
+let contadorEmpates = 0
+let contadorGanados= 0
+let contadorPerdidos= 0
 
 pcOpcion = () =>{
   let eleccion = opciones[numeroAleatorio()];
@@ -33,22 +40,81 @@ jugadorOpcion = (opcion) => {
   juego(opcion, xd)
 }
 
+const avisoResultado = (resultado) => {
+  const anuncioGANASTE = document.getElementById('anuncioGANASTE');
+  const anuncioEMPATE = document.getElementById('anuncioEMPATE');
+  const anuncioPERDISTE = document.getElementById('anuncioPERDISTE');
+  const anuncioNoJugado = document.getElementById('anuncioNoJugado');
+
+  anuncioGANASTE.classList.add("hidden");
+  anuncioEMPATE.classList.add("hidden");
+  anuncioPERDISTE.classList.add("hidden");
+  anuncioNoJugado.classList.add("hidden");
+
+  const anuncioAMostrar = document.getElementById(`anuncio${resultado}`);
+  anuncioAMostrar.classList.remove('hidden');
+}
+
+const registroJuego = (resultado, jugadorOpcion, pcOpcion) =>{
+  const registroContainer = document.getElementById('registroContainer');
+
+  let borderClass = '';
+  let textClass = '';
+
+  if(resultado === 'GANASTE'){
+    borderClass = 'border-[#3fb16d]';
+    textClass = 'text-[#3fb16d] text-shadow-md text-shadow-[#3fb16d]';
+  }
+  else if (resultado === 'PERDISTE'){
+    borderClass = 'border-[#d47677]';
+    textClass = 'text-[#d47677] text-shadow-md text-shadow-[#d47677]';
+  }
+  else{
+    borderClass = 'border-[#94a3b8]';
+    textClass = 'text-[#94a3b8] text-shadow-md text-shadow-[#94a3b8]';
+  }
+
+  jugadorOpcion = 
+    jugadorOpcion === 'fa-hand-fist' ? 'PIEDRA' :
+    jugadorOpcion === 'fa-hand' ? 'PAPEL' :
+    'TIJERA';
+
+  pcOpcion = 
+    pcOpcion === 'fa-hand-fist' ? 'PIEDRA' :
+    pcOpcion === 'fa-hand' ? 'PAPEL' :
+    'TIJERA';
+
+  const registro = document.createElement('div');
+  registro.className = `border-l-4 ${borderClass} rounded-sm px-2 py-1`;
+  registro.innerHTML = `
+    <p class="text-[#94a3b8] text-shadow-md text-shadow-[#94a3b8]"><span class="text-[#00ffff] text-shadow-md text-shadow-[#00ffff]">${jugadorOpcion}</span> vs <span class="text-[#ff00ff] text-shadow-md text-shadow-[#ff00ff]">${pcOpcion}</span></p>
+    <p class="${textClass}">${resultado}</p>
+  `;
+  registroContainer.appendChild(registro);
+}
+
 const juego = (jugadorOpcion, pcOpcion) => {
   if(jugadorOpcion === pcOpcion){
-    console.log(`Jugador: ${jugadorOpcion}. PC: ${pcOpcion}.`);
-    console.log("Empate");
+    avisoResultado('EMPATE')
+    registroJuego('EMPATE', jugadorOpcion, pcOpcion);
+    contadorEmpates = parseInt(empatados.textContent) + 1;
+    empatados.textContent = contadorEmpates;
   } 
   else if(
     jugadorOpcion === "fa-hand" && pcOpcion === "fa-hand-fist" ||
     jugadorOpcion === "fa-hand-scissors" && pcOpcion === "fa-hand" ||
     jugadorOpcion === "fa-hand-fist" && pcOpcion === "fa-hand-scissors"
   ){
-    console.log(`Jugador: ${jugadorOpcion}. PC: ${pcOpcion}.`);
-    console.log("Ganador: Jugador");
+    avisoResultado('GANASTE')
+    registroJuego('GANASTE', jugadorOpcion, pcOpcion);
+    contadorGanados = parseInt(ganados.textContent) + 1;
+    ganados.textContent = contadorGanados;
   }
   else{
-    console.log(`Jugador: ${jugadorOpcion}. PC: ${pcOpcion}.`);
-    console.log("Ganador: PC");
+    avisoResultado('PERDISTE')
+    registroJuego('PERDISTE', jugadorOpcion, pcOpcion);
+    contadorPerdidos = parseInt(perdidos.textContent) + 1;
+    perdidos.textContent = contadorPerdidos;
   }
 }
 
